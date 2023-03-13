6efcac525c40bb0ff3fd36bdd97a6fb715b41a87
Naldo92i <92037525+Naldo92i@users.noreply.github.com>
2023-03-13 17:09:15 +0000

    Connection github configuré avec succès

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,8 @@
     </head>
     <body>
         <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
-            {{ $slot }}
+            {{-- {{ $slot }} --}}
         </div>
+        @yield('social')
     </body>
 </html>
